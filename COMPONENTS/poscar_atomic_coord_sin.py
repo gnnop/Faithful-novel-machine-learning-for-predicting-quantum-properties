7756fcfdcb59761d954636eed4ab35de08893d86
gnnop <63081359+gnnop@users.noreply.github.com>
2024-07-01 19:12:11 -0500

    Update poscar_atomic_coord_sin.py
    now the atom_coords are coords only

--- COMPONENTS/poscar_atomic_coord_sin.py
+++ COMPONENTS/poscar_atomic_coord_sin.py
@@ -19,12 +19,4 @@ def info(poscar):
         if curIndx > numbs[atomType]:
             atomType+=1
         
-        atomRep = atomToCoords(atoms[atomType])
-        rw = [0.0]*7
-        cl = [0.0]*16
-        sp = [0.0]*1
-        rw[atomRep[0]] = 1.0
-        cl[atomRep[1]] = 1.0
-        sp[0] = atomRep[2]#The spin is a single bit, so we drop it in one thing
-        #We have the whole representation right now - after this I add it into the graph for clarity
         nodesArray.append([getRelativeCoordinates(i) for i in unpackLine(poscar[8+i])])
