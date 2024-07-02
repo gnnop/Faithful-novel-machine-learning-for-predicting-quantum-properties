7079ead23d7636b474e272fc5a1fca830026a1b6
gnnop <63081359+gnnop@users.noreply.github.com>
2024-07-02 12:40:45 -0500

    updating crossmodelcompatbiility

--- MODELS/NNN.py
+++ MODELS/NNN.py
@@ -66,7 +66,7 @@ def net_fn(batch, is_training=False, dropout_rate=0):
 
 output_dim = 0#initialize before calling the network
 
-if not exists("processed.pickle"):
+if not exists("NNN.pickle"):
 
     #the atomic embeddings are assumed to be both input and available for all
     #poscar files. The global embeddings are assumed to not exist for all elements.
@@ -143,7 +143,7 @@ if not exists("processed.pickle"):
 
     print(ii, "poscars were skipped due to having more than", hp["max_atoms"], "atoms.")
 
-    with open("processed.pickle", "wb") as f:
+    with open("NNN.pickle", "wb") as f:
         pickle.dump({
             "data": np.concatenate((np.array(pa), np.array(pi)), axis=1),
             "labels": np.array(go),
@@ -152,7 +152,7 @@ if not exists("processed.pickle"):
         },f)
 
 
-with open("processed.pickle", "rb") as f:
+with open("NNN.pickle", "rb") as f:
     data = pickle.load(f)
     inputs = data["data"]
     labels = data["labels"]
