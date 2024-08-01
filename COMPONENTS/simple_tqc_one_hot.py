ff3da3ddb2e8b2190c62641578aed1295c60f761
gnnop <63081359+gnnop@users.noreply.github.com>
2024-08-01 12:56:11 -0500

    Update simple_tqc_one_hot.py to use one csv

--- COMPONENTS/simple_tqc_one_hot.py
+++ COMPONENTS/simple_tqc_one_hot.py
@@ -1,27 +1,26 @@
 from includes import *
+# Create an instance of the loader to keep the CSV loaded
+csv_loader = CSVLoader('advanced_tqc.csv')
 
-# create an instance of the loader to keep the csv loaded
-csv_loader = CSVLoader("simple_tqc.csv")
-
-# functions to be used in another module
-
-
+# Functions to be used in another module
 def valid_ids():
     return csv_loader.valid_ids()
 
-
 def classifier():
-    return EvaluationMethods.classification
-
+    return loss_classification
 
 def info(id):
     temp = csv_loader.info(id)
-    arr = [0.0] * 3
-    if temp == "trivial":
-        arr[0] = 1.0
-    elif temp == "TI":
-        arr[1] = 1.0
-    elif temp == "SM":
-        arr[2] = 1.0
+    arr = [0.0]*3
+    if temp == "ES":
+        arr[0] = 1.0#TSM
+    elif temp == "ESFD":
+        arr[0] = 1.0#TSM
+    elif temp == "NLC":
+        arr[1] = 1.0#TI
+    elif temp == "SEBR":
+        arr[1] = 1.0#TI
+    elif temp == "LCEBR":
+        arr[2] = 1.0#trivial
 
     return arr
